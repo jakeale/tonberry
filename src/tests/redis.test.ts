b49618a58c0e881557a8763999edacaa9d4689b6
Jake Alexander <jake.ale@outlook.com>
2021-10-30 16:01:18 -0700

    Add Redis to CI workflow (#2)
    * Add Redis startup tests

---
+++ src/tests/redis.test.ts
@@ -0,0 +1,9 @@
+import { startRedis } from '../redis';
+
+test('Redis startup', async () => {
+  const client = await startRedis();
+
+  expect(await client.ping()).toBe('PONG');
+
+  await client.quit();
+});
