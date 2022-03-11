67d9869866f8a55e67dc3601aef49a98057d2508
Jake Alexander <jake.ale@outlook.com>
2022-03-10 20:40:33 -0800

    remove `status`
    * no longer using this approach

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Check if a given object is empty.
- * @param obj
- * @returns True if empty, else False
- */
-export const isEmpty = (obj: object) => {
-  return Object.keys(obj).length === 0;
-};
